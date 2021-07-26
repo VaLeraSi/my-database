28be7772107a24272e92695b9f7552cada2c5a0b
VaLeraSi <74680565+VaLeraSi@users.noreply.github.com>
2021-07-26 17:09:15 +0300

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,84 @@
+
+-- √руппировка 
+
+use online_school;
+
+select name, MAX(cost) as 'средн€€ ценова€ категори€'
+FROM study_programs
+where cost>=100000
+GROUP BY name;
+
+-- JOIN
+
+use online_school;
+
+select 
+	profiles.student_id as student_id, study_programs.id as study_program_id, study_programs.cost as cost, profiles.study_program_id as profiles_study_program_id
+from profiles 
+inner join study_programs on study_programs.id=profiles.study_program_id;
+	
+
+-- ¬ложенный запрос
+
+use online_school;
+
+select student_id
+from scores
+where mark>=5;
+
+
+-- представлени€ 
+-- 1
+
+use online_school;
+
+CREATE view rev  
+as select * from reviews 
+order by evaluation;
+SELECT * FROM rev;
+
+-- 2
+use online_school;
+
+CREATE view new_rev
+as select body, evaluation from reviews;
+SELECT * FROM new_rev;
+
+
+-- хранимые процедуры 
+
+use online_school;
+
+DELIMITER //
+
+CREATE procedure timetable_for_teacher()
+begin 
+	select subject_id, teacher_id, start_time
+	from timetable
+	where teacher_id=10;
+end//
+
+delimiter ;
+
+call timetable_for_teacher();
+
+-- триггеры 
+
+use online_school;
+
+DELIMITER //
+
+CREATE trigger after_dismissal_teacher
+AFTER delete
+	on teachers FOR EACH row
+begin 
+	delete from timetable
+	where teacher_id=old.id; 
+end//
+
+DELIMITER ;
+
+
+
+
+
